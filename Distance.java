fa7f342cffb6ba8c77a372bf8b322346a8dfd761
COMP3220-G12 <68089001+COMP3220-G12@users.noreply.github.com>
2020-07-14 02:22:56 -0400

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -5,6 +5,8 @@ public class Distance {
     float lat1;
     float lon1;
     List<Centres> cCentres;
+
+    //overloaded constructor to set values
     public Distance(float lat1, float lon1, List<Centres> cCentres)
     {
         this.lat1 = lat1;
@@ -12,14 +14,18 @@ public class Distance {
         this.cCentres = cCentres;
 
     }
+
+    //create a list for the nearest community centres
     public List<Centres> near(List<Centres> cCentres)
     {
         List<Centres> closest = new ArrayList<Centres>();
         for(Centres i: cCentres)
         {
+            //latitude and longitude for each centre
             float lat2 = i.getY();
             float lon2 = i.getX();
-            double dist = findNearest(lat2, lon2, cCentres);
+            //distance
+            double dist = calcDist(lat2, lon2, cCentres);
             if(dist<=5)
             {
                 Centres near = new Centres(i.getFID(), i.getAddress(), i.getname(), i.getX(), i.getY(), dist);
@@ -28,7 +34,9 @@ public class Distance {
         }
         return closest;
     }
-    public double findNearest(float lat2, float lon2, List<Centres> cCentres)
+
+    //calculate distances
+    public double calcDist(float lat2, float lon2, List<Centres> cCentres)
     {
         double theta = lon1 - lon2;
         double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
