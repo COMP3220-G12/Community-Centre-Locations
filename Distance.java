78063429e7bf0e1d1ece00942caa3143bd689fcb
COMP3220-G12 <68089001+COMP3220-G12@users.noreply.github.com>
2020-07-15 18:00:53 -0400

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -38,12 +38,17 @@ public class Distance {
     //calculate distances
     public double calcDist(float lat2, float lon2, List<Centres> cCentres)
     {
-        double theta = lon1 - lon2;
-        double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
-        dist = Math.acos(dist);
-        dist = Math.toDegrees(dist);
-        dist = dist * 60 * 1.1515;
-        dist = dist * 1.609344;     //km
+        double dist = 0;
+        if (lat1==lat2 && lon1==lon2)
+            dist = 0;
+        else {
+            double theta = lon1 - lon2;
+            dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            dist = dist * 1.609344;     //km}
+        }
         return (dist);
     }
 
